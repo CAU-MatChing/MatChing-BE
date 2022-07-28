d457638332802cd127d9ecacb63ae91e4858b512
na-yk <ninghao17@naver.com>
2022-07-28 13:58:03 +0900

    makemigrations and migrate

--- matching_BE/matchings/urls.py
+++ matching_BE/matchings/urls.py
@@ -3,8 +3,8 @@ from .views import *
 
 urlpatterns = [ 
     path('create-matching/', create_matching, name='create-matching'),
-    path('delete-matching/<int:id>', delete_matching, name='delete-matching'),
-    path('update-matching/<int:id>', update_matching, name='update-matching'),
-    path('get-matching/<int:id>', get_matching, name='get-matching'),
-    path('get-all-matching/', get_all_matching, name='get-all-matching'),
+    # path('delete-matching/<int:id>', delete_matching, name='delete-matching'),
+    # path('update-matching/<int:id>', update_matching, name='update-matching'),
+    # path('get-matching/<int:id>', get_matching, name='get-matching'),
+    # path('get-all-matching/', get_all_matching, name='get-all-matching'),
 ]
