9a2b7543a448ad39420d1bf7ef59b4eb6c0607e1
na-yk <ninghao17@naver.com>
2022-07-28 13:57:01 +0900

    feat: Add accounts management admin 페이지에서 이메일을 아이디로 하여 관리할 수 있도록 만들었음

--- matching_BE/accounts/models.py
+++ matching_BE/accounts/models.py
@@ -1,11 +1,62 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager
 
 # Create your models here.
 # cy : change email, password null=True
 
 # 수정해야댐
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('이메일 주소가 필요합니다.')
+
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.is_admin = True
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+
 class Account(AbstractBaseUser):
     email = models.CharField(max_length=50,unique=True,null=True)
-    #password = models.CharField(max_length=20,null=True)
+    # password = models.CharField(max_length=20,null=True)
     is_active = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
