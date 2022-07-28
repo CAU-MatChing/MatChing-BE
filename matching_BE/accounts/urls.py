9bc61846ffae49e926ff66b508c094913b744a7c
na-yk <ninghao17@naver.com>
2022-07-28 18:17:10 +0900

    feat: Add login 기능

--- matching_BE/accounts/urls.py
+++ matching_BE/accounts/urls.py
@@ -1,12 +1,12 @@
 # cy : add new file
 
 from django.urls import path
-from . import views
+from .views import *
    
 urlpatterns = [
-    path('signup/', views.SignUp.as_view(), name = 'signup'),
-    path('activate/<str:uidb64>/<str:token>/', views.Activate.as_view(), name = 'activate'),
-    #path('login/',views.LogIn.as_view(), name = 'login'),
-    #path('logOut/',views.LogOut.as_view(), name = 'logout'),
+    path('signup/', SignUp.as_view(), name = 'signup'),
+    path('activate/<str:uidb64>/<str:token>/', Activate.as_view(), name = 'activate'),
+    path('login/',login, name = 'login'),
+    # path('logout/',logout, name = 'logout'),
     #path('withdraw/',views.Withdraw.as_view(), name = 'withdraw'),
 ]
