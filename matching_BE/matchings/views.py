f250d5e881750fc4318de5f11912538bded649d2
na-yk <ninghao17@naver.com>
2022-08-28 21:49:23 +0900

    style: Remove comments

--- matching_BE/matchings/views.py
+++ matching_BE/matchings/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_http_methods
-from django.http import JsonResponse,HttpResponse
+from django.http import HttpResponse
 from .models import Matching, Follower
 from profiles.models import Profile
 from matzips.models import Matzip
--- matching_BE/matzips/urls.py
+++ matching_BE/matzips/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('', create_getall_matzip, name='create_getall_matzip'),
     path('now/', getall_now_matzip, name='getall_now_matzip'),
     path('<int:id>/', get_update_delete_matzip, name='get_update_delete_matzip'),
-
 ]
