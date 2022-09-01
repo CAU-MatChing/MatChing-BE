93cde35d384879c32e0f33977ceded64041f4c4e
na-yk <ninghao17@naver.com>
2022-09-02 01:15:29 +0900

    chore: Delete location field 위치 데이터 필드 삭제

--- matching_BE/matzips/models.py
+++ matching_BE/matzips/models.py
@@ -3,5 +3,4 @@ from django.db import models
 # Create your models here.
 class Matzip(models.Model):
     name = models.CharField(max_length=60,null=True,default='', unique=True)
-    location = models.TextField(blank=True,default='')
     waiting = models.IntegerField(default=0)
