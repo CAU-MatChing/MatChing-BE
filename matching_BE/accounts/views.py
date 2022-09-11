86be3e4cceb44941ac2af626c15a11edc5258af1
na-yk <ninghao17@naver.com>
2022-09-11 14:14:39 +0900

    update: Update activate view redirect url

--- matching_BE/accounts/views.py
+++ matching_BE/accounts/views.py
@@ -108,7 +108,7 @@ def activate(request, uidb64, token) :
                 user.is_active = True
                 user.save()
 
-                return redirect('https://github.com/hectick') #리다이렉트 페이지
+                return redirect('http://localhost:3000/signup/success') #리다이렉트 페이지
         
             return JsonResponse(
                 {
